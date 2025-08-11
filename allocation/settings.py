e1ffa4813823c9e915e6e01a099622bab1b136de
Nardi <fjnf@outlook.com>
2025-08-10 22:23:23 -0300

    Add Dockerfile for containerized deployment

--- allocation/settings.py
+++ allocation/settings.py
@@ -1,6 +1,7 @@
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication', 
+        'rest_framework.authentication.BasicAuthentication',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
 }
 API_KEY = 'abcdef123456'  
